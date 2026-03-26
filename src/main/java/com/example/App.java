62f615429c84c3df58d6d6362dc47568711166e6
user <user@user.user>
2026-03-25 21:58:35 -0700

    Keep Java app running for Kubernetes deployment

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,8 +1,12 @@
 package com.example;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello from Java app");
+
+        while (true) {
+            Thread.sleep(60000);
+        }
     }
 
     public static int add(int a, int b) {
